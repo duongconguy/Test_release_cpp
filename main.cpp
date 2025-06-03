d34272e204b0d41600fb0b5482020477dcb81eed
duongconguy <112960009+duongconguy@users.noreply.github.com>
2025-06-04 00:31:47 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,54 +1,34 @@
-#define UNICODE
-#include <windows.h>
+#include <iostream>
 
-// Hàm xử lý sự kiện
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
+using namespace std;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == 1 && HIWORD(wParam) == BN_CLICKED) {
-            MessageBox(hwnd, L"Bạn đã bấm nút!", L"Thông báo", MB_OK);
+int main() {
+    while (true) {
+        cout << "============================" << endl;
+        cout << "CHUONG TRINH QUAN LY VI DIEN TU" << endl;
+        cout << "1. Dang nhap" << endl;
+        cout << "2. Dang ky" << endl;
+        cout << "3. Thoat" << endl;
+        cout << "Chon: ";
+
+        int choice;
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Dang nhap - chua ho tro" << endl;
+                break;
+            case 2:
+                cout << "Dang ky - chua ho tro" << endl;
+                break;
+            case 3:
+                cout << "Tam biet!" << endl;
+                return 0;
+            default:
+                cout << "Lua chon khong hop le!" << endl;
         }
-        break;
-    }
 
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        cout << endl;
+    }
 }
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
-    const wchar_t CLASS_NAME[] = L"SimpleWindowClass";
-
-    WNDCLASSW wc = {};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
-
-    RegisterClassW(&wc);
-
-    HWND hwnd = CreateWindowExW(
-        0, CLASS_NAME, L"Cửa sổ đơn giản", WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 300, 150,
-        NULL, NULL, hInstance, NULL
-    );
-
-    if (hwnd == NULL) return 0;
-
-    CreateWindowW(L"BUTTON", L"Bấm vào đây",
-        WS_VISIBLE | WS_CHILD,
-        80, 40, 120, 30,
-        hwnd, (HMENU)1, hInstance, NULL);
-
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
-
-    MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return 0;
-}
